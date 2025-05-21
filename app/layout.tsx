9302d728f5379295341c4196206854664204e992
Rukundo-Bahati <rukundorca@gmail.com>
2025-05-21 08:31:03 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,15 @@ import "./globals.css"
 import { ThemeProvider } from "@/app/context/ThemeContext" // path to your ThemeContext file
 import { AuthProvider } from "@/app/context/AuthContext"
 
+// app/layout.tsx (App Router)
+export const metadata = {
+  title: 'HotelMS',
+  icons: {
+    icon: '/favicon.png',
+  },
+};
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
